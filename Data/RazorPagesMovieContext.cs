cb8f9f41850b53cc6ccd3c0de0670744b738d1c4
SubhanMaulana <SubhanMaulana@DESKTOP-J9MOMED>
2022-06-24 14:45:35 +0700

    Add project files.

---
+++ Data/RazorPagesMovieContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Data
+{
+    public class RazorPagesMovieContext : DbContext
+    {
+#pragma warning disable CS8618
+        public RazorPagesMovieContext (DbContextOptions<RazorPagesMovieContext> options)
+#pragma warning restore CS8618
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie.Models.Movie>? Movie { get; set; }
+    }
+}
---
+++ Pages/Movies/Delete.cshtml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Pages.Movies
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
+
+        public DeleteModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+      public Movie Movie { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Movie == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Movie = movie;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Movie == null)
+            {
+                return NotFound();
+            }
+            var movie = await _context.Movie.FindAsync(id);
+
+            if (movie != null)
+            {
+                Movie = movie;
+                _context.Movie.Remove(Movie);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
+
+#pragma warning disable CS8618
+#pragma warning disable CS8601
+#pragma warning disable CS8602
+#pragma warning disable CS8604
+// Class
+#pragma warning restore CS8618
+#pragma warning restore CS8601
+#pragma warning restore CS8602
+#pragma warning restore CS8604
